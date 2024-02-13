0c0fe32a26a06b4acbfd35ad2749865060bbd8d8
OckJuYong <20201776@edu.hanbat.ac.kr>
2024-02-13 21:14:35 +0900

    hot

--- src/component/Task/Assingment.js
+++ src/component/Task/Assingment.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams, useNavigate } from 'react-router-dom';
+import cookie from 'react-cookies';
 
+import taskImg from './image 8.png';
+
+import MenuHeader from '../Main/header/MenuHeader';
+import LogoHeader from '../Main/header/LogoHeader';
+
+import './Assingment.css';
 
 
 // 파일(file), 제출시간(submission_time), 
@@ -33,11 +40,7 @@ useEffect(() => {
     try {
       const response = await axios.get(`${address}/week/`, student_id);
       setWeekList(response.data);
-      
       setWeekCount(Math.ceil(response.data.length / userCount)); // 올림으로 변경
-
-
-      
     } catch (error) {
       console.error('Error fetching week list:', error);
     }
@@ -45,6 +48,7 @@ useEffect(() => {
 
   fetchData();
 }, []);
+
 useEffect(() => {
   console.log("weekList:", weekList); // weekList 출력
   if (weekList.length > 0) {
@@ -65,7 +69,7 @@ console.log(myWeekList);
 
 useEffect(() => {
   const fetchData = async () => {
-    for (let i = 1; i < weekCount + 1; i++) {
+    for (let i = 1; i < (weekCount + 1)/2; i++) {
       try {
         const response = await axios.get(`${address}/users/${i}/`);
         console.log(`${address}/users/${i}/`);
@@ -121,28 +125,117 @@ console.log("유저정보",userTaskData);
     }
   }, []);
 
+
+  const LoginAddress = 
+  // "https://port-0-djangoproject-umnqdut2blqqevwyb.sel4.cloudtype.app/login/";
+  "http://15.164.190.171/login/";
+
+  const [accessToken, setAccessToken] = useState(null);
+  const [refreshToken, setRefreshToken] = useState(null);
+
+  const [userName, setUserName] = useState(null);
+  const [userDivision, setUserDivision] = useState(null);
+  const [student_Id, setStudent_Id] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const savedAccessToken = cookie.load("accessToken");
+        const savedRefreshToken = cookie.load("refreshToken");
+        setRefreshToken(savedRefreshToken);
+        setAccessToken(savedAccessToken);
+  
+        console.log("Trying to get access token...");
+  
+        const getAccessTokenResponse = await axios.post(
+          `${LoginAddress}`,
+          {
+            access: savedAccessToken,
+            refresh: savedRefreshToken,
+          }
+        );
+  
+        setUserName(getAccessTokenResponse.data.name);
+        setUserDivision(getAccessTokenResponse.data.division);
+        setStudent_Id(getAccessTokenResponse.data.username)
+        console.log(getAccessTokenResponse.data.name);
+        console.log(getAccessTokenResponse.data.username);
+        console.log(getAccessTokenResponse.data.division);
+
+        
+        
+        cookie.save("accessToken", getAccessTokenResponse.data.access, {
+          path: "/",
+          expires: new Date(getAccessTokenResponse.data.expires),
+        });
+      } catch (error) {
+        console.error("Error checking access token:", error);
+      }
+    };
+  
+    // Call fetchData function
+    fetchData();
+  }, []); // Empty dependency array for the initial render only
+
+  useEffect(() => {
+    const fetchDataWrapper = async () => {
+      const savedAccessToken = await cookie.load('accessToken');
+      const savedRefreshToken = await cookie.load('refreshToken');
+
+      setAccessToken(savedAccessToken);
+      setRefreshToken(savedRefreshToken);
+
+
+      // Assuming fetchData is a function that you've defined elsewhere
+      // await fetchData(accessAddress, savedAccessToken, savedRefreshToken, setAccessToken, setRefreshToken);
+    };
+
+    fetchDataWrapper();
+  }, []);
+
   
 
   console.log(assignment);
-  return (
-    <>
-      <div>과제 제출페이지</div>
-      <Link to={`/TaskEdit/${urlNumber}`}>과제 제출하기</Link>
-      <div>
-        <h2>Assignment Information</h2>
-        <ol>
-          {assignment.map((outerItem, outerIndex) => (
-            <li key={outerIndex}>
-              <h3>{outerItem.assignment_title}</h3>
-              <p>file: {outerItem.file}</p>
-              <p>Submission Status: {outerItem.submission_time}</p>
-            </li>
-          ))}
-        </ol>
-        <button onClick={() => assignmentdelte()}>과제 삭제하기</button>
-      </div>
-    </>
-  );
+  const uniqueItems = myWeekList.filter((item, index, self) =>
+  index === self.findIndex(t => (
+    t.assignment_title === item.assignment_title && t.file === item.file
+  ))
+);
+
+const weekNumber = urlNumber + 1; // 가져오고자 하는 week의 번호
+console.log(weekNumber);
+
+
+
+return (
+  <>
+    <MenuHeader />
+    <LogoHeader/>
+    <div className='assingment__title'></div>
+    <div>
+      <h1 className='assingment__title__week__header'>{urlNumber + 1} 주차</h1>
+      <Link className="assingment__plus__button" to={`/TaskEdit/${urlNumber}`}>과제 제출하기</Link>
+      <div className='task__header__line'></div>
+      <ul>
+        {uniqueItems.map((item, index) => (
+          <li key={index} className='Assingment__weeks__main__container'>
+            <img className="Assingment__img__yo" src={taskImg}></img>
+            <div className='Assingment__li__main__container'>
+              <p>File: {student_Id}.{item.file.split('/').pop().split('.').pop()}</p>
+              <p>{item.submission_time.split("T")[0].replace(/-/g, ".")}</p>
+            </div>
+            <button className="assingment__del__button" onClick={() => assignmentdelte(item.assignment_id)}>과제 삭제하기</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </>
+);
+
+
+  
+  
+    
 };
 
 export default Assignment;
